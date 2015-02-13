0676ff678263e756c8a1dc21d36bdd8d6244d495
Jose Ricardo Pascarella Quijada <josepas@panchita.ac.labf.usb.ve>
2015-02-12 19:49:38 -0430

    terminando el pasaje de argumentos

--- Proyecto2/usoDisco.c
+++ Proyecto2/usoDisco.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 
 int main(int argc, char *argv[]) {
-    int aflag = 0;
-    int bflag = 0;
+
     char* directorio = NULL;
     char* salida;
     int index;
-    char cwd[500];
+    char buff[500];
     int nHijos = 0;
     int c;
 
@@ -19,6 +19,10 @@ int main(int argc, char *argv[]) {
         switch (c) {
             case 'h':
                 printf("UsoDisco [-h] | [-n i] [-d directorio] salida\n");
+                printf("\n");
+                printf("-h  Muestra esta informacion de uso.\n");
+                printf("-n  Numero de procesos a usar.\n");
+                printf("-d  Directorio al que se aplica el programa.\n");
                 exit(0);
             case 'd':
                 directorio = optarg;
@@ -28,23 +32,23 @@ int main(int argc, char *argv[]) {
                 break;
             case '?':
                 if (optopt == 'n')
-                    fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+                    fprintf (stderr, "Opcion -%c requiere un argumento.\n", optopt);
                 else if (optopt == 'd')
-                    fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+                    fprintf (stderr, "Opcion -%c requiere un argumento.\n", optopt);
                 else
-                    fprintf (stderr, "Unknown option character %x'.\n", optopt);
+                    fprintf (stderr, "Opcion desconocida -%c.\n", optopt);
             default:
-                exit(0);
+                exit(1);
         }
     }
     salida = argv[optind];
-    if (directorio == NULL) {
+    if (directorio == NULL)
+        directorio = getcwd(buff, sizeof(buff));
+    if (salida == NULL) {
+        printf("Archivo de salida no especificado\n");
+        exit(1);
+    }
 
-        getcwd(directorio, 500);
-        
-        printf("%s\n", directorio);
-
-    } 
     printf ("directorio = %s, nHijos = %d salida = %s.\n", directorio, nHijos, salida );
 
     return 0; 
